d912f08524d9acecced79f4cafe5436fb197e38c
yusufjk9 <aynasyusuf5@gmail.com>
2026-06-04 23:35:14 +0300

    QR kod scriptini güncelledim

--- create_qr.py
+++ create_qr.py
@@ -1,10 +1,7 @@
-import os
-
 import qrcode
 
-menu_url = os.getenv("MENU_URL") or "https://coruscating-starlight-08a7a7.netlify.app"
+menu_url = "https://yusufaynas.github.io/qr-menu"
 
-# QR kod oluştur
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -14,7 +11,6 @@ qr = qrcode.QRCode(
 qr.add_data(menu_url)
 qr.make(fit=True)
 
-# Resim oluştur
 img = qr.make_image(fill_color="black", back_color="white")
 img.save('qr_code.png')
 
